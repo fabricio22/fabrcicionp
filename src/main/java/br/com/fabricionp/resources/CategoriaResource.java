ce464c591e646c3d8b650f8c242d17fff7eedea8
Fabricio Nascimento Pires <fanpir13@hotmail.com>
2021-06-09 01:24:03 -0300

    Criado dominio e alterado retorno do rest para listagem de categoria

--- src/main/java/br/com/fabricionp/resources/CategoriaResource.java
+++ src/main/java/br/com/fabricionp/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package br.com.fabricionp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fabricionp.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria categoria = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+
+		List<Categoria> listaCategoria = new ArrayList<Categoria>();
+		listaCategoria.add(categoria);
+		listaCategoria.add(categoria2);
+		
+		return listaCategoria;
 	}
 }
