dd0ac4acd61f60a313295a8715d429c476753238
Fabricio Nascimento Pires <fanpir13@hotmail.com>
2021-06-09 11:17:30 -0300

    Criado entity de categoria

--- src/main/java/br/com/fabricionp/domain/Categoria.java
+++ src/main/java/br/com/fabricionp/domain/Categoria.java
@@ -2,9 +2,17 @@ package br.com.fabricionp.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 5902624989392120958L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
